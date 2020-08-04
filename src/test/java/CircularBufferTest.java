1988907d87a3373f1501aacf85792c0b5150d18e
Phatthanan Rittichai <phatthananrit@mathregit.group>
2020-08-04 10:44:27 +0700

    tennis1

--- src/test/java/CircularBufferTest.java
+++ src/test/java/CircularBufferTest.java
@@ -4,26 +4,34 @@ import static org.junit.Assert.*;
 
 public class CircularBufferTest {
     @Test
-    public void create_new_buffer_should_empty(){
+    public void create_new_buffer_should_empty() {
         CircularBuffer cb = new CircularBuffer();
         boolean result = cb.isEmpty();
-        assertTrue("Buffer not empty",result);
+        assertTrue("Buffer not empty", result);
     }
+
     @Test
-    public void create_new_buffer_with_default_size_should_10(){
+    public void create_new_buffer_with_default_size_should_10() {
         CircularBuffer cb = new CircularBuffer();
-        for(int i=0;i<10;i++){
-            cb.writeData("A"+i);
+        for (int i = 0; i < 10; i++) {
+            cb.writeData("A" + i);
         }
         boolean result = cb.isFull();
         assertTrue("Buffer not emoty naja ", result);
     }
+
     @Test
-    public void write_A_to_buffer_should_read_A(){
-        CircularBuffer cb  = new CircularBuffer();
+    public void write_A_to_buffer_should_read_A() {
+        CircularBuffer cb = new CircularBuffer();
         cb.writeData("A");
         cb.writeData("B");
-        assertEquals("A" , cb.readData());
-        assertEquals("B" , cb.readData());
+        assertEquals("A", cb.readData());
+        assertEquals("B", cb.readData());
+    }
+
+    @Test
+    public void write_B_to_buffer_should_read_A() {
+        CircularBuffer cb = new CircularBuffer();
+
     }
 }
